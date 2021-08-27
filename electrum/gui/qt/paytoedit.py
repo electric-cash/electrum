b0ccf9408231853b4e9efcd51617002e7bddc9f0
dol3 <75476445+dol3@users.noreply.github.com>
2021-08-27 12:14:30 +0200

    fix(qr): new qrreader using QtMultimedia; drop CalinsQRReader(mac) (#58)
    * fix(qr): new qrreader using QtMultimedia; drop CalinsQRReader(mac)
    
    * fix( qrreader): keep both old and new toolchain; try to abstract it away
    
    * fix(qr): add qrreader
    
    * chore: update requirements importlib-metadata

--- electrum/gui/qt/paytoedit.py
+++ electrum/gui/qt/paytoedit.py
@@ -269,11 +269,12 @@ class PayToEdit(CompletionTextEdit, ScanQRTextEdit, Logger):
         self.setMaximumHeight(h)
         self.verticalScrollBar().hide()
 
-    def qr_input(self):
-        data = super(PayToEdit,self).qr_input()
-        if data.find('?') > 0:
-            self.win.pay_to_URI(data)
-            # TODO: update fee
+    def qr_input(self, *, callback=None):
+        def _on_qr_success(data):
+            if data.find('?') > 0:
+                self.win.pay_to_URI(data)
+                # TODO: update fee
+        super(PayToEdit, self).qr_input(callback=_on_qr_success)
 
     def resolve(self):
         self.is_alias = False
