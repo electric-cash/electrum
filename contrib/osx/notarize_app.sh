ffc661339b57ff49e15fb9f8940c87bd10f28c9f
Piotr Sarzyński <pickap@gmail.com>
2022-06-14 13:15:24 +0200

    build(macos): add workflow for macos

--- contrib/osx/notarize_app.sh
+++ contrib/osx/notarize_app.sh
@@ -28,6 +28,7 @@ RESULT=$(xcrun altool --notarize-app --type osx \
   --primary-bundle-id org.electrum.electrum \
   --username $APPLE_ID_USER \
   --password @env:APPLE_ID_PASSWORD \
+  --asc-provider @env:APPLE_ID_PROVIDER_SHORT_NAME \
   --output-format xml)
 
 if [ $? -ne 0 ]; then
